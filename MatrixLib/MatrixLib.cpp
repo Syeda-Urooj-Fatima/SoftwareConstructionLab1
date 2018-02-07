9274a0f56d46869f65e20ad61ca6d23401516c49
Syeda Urooj Fatima <suroojfatima97@gmail.com>
2018-02-07 12:54:21 +0500

    Updated multiplication

--- MatrixLib/MatrixLib.cpp
+++ MatrixLib/MatrixLib.cpp
@@ -4,3 +4,28 @@
 
 #include "MatrixLib.h"
 
+namespace MatrixLib
+{
+	int** Class1::multiply(int a[3][3], int b[3][3])
+	{
+		int** resultarray = 0;
+		resultarray = new int*[3];
+		for (int i = 0; i < 3; i++)
+		{
+			resultarray[i] = new int[3];
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				resultarray[i][j] = 0;
+				for (int k = 0; k < 3; k++)
+				{
+					resultarray[i][j] = resultarray[i][j] + a[i][k] * b[k][j];
+				}
+			}
+		}
+		return resultarray;
+	}
+}
--- MatrixLib/MatrixLib.h
+++ MatrixLib/MatrixLib.h
@@ -9,5 +9,8 @@ namespace MatrixLib {
 	public ref class Class1
 	{
 		// TODO: Add your methods for this class here.
+		public:
+			// Returns a + b  
+			static int** multiply(int a[3][3], int b[3][3]);
 	};
 }
