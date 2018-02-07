5cf337b456e032531d14b4898584bb67767bdb1a
Syeda Urooj Fatima <suroojfatima97@gmail.com>
2018-02-07 13:40:03 +0500

    Updated

--- MatrixLib/MatrixLib.cpp
+++ MatrixLib/MatrixLib.cpp
@@ -3,9 +3,35 @@
 #include "stdafx.h"
 
 #include "MatrixLib.h"
+#include <iostream>
+
+using namespace std;
 
 namespace MatrixLib
 {
+		int** Class1::addition(int x[3][3], int y[3][3]){
+			int** z = new int*[3];
+			// Adding Both Matrices
+			for (int i = 0; i<3; i++)
+			{
+				for (int j = 0; j<3; j++)
+					z[i][j] = x[i][j] + y[i][j];
+			}
+			return z;
+		}
+
+		int** Class1::subtract(int x[3][3], int y[3][3]){                // Subtarction Function implementation
+			int** z = new int*[3];
+			int i, j;
+			
+			for (i = 0; i<3; i++)
+			{
+				for (j = 0; j<3; j++)
+					z[i][j] = x[i][j] - y[i][j];
+			}
+			
+			return z;
+		}
 	int** Class1::multiply(int a[3][3], int b[3][3])
 	{
 		int** resultarray = 0;
@@ -27,5 +53,19 @@ namespace MatrixLib
 			}
 		}
 		return resultarray;
+	}
+
+	int** Class1::inverse(int a[3][3])
+	{
+		int r = 3; int c = 3; int** trans = new int*[3];
+		for (int i = 0; i < r; ++i)
+		for (int j = 0; j < c; ++j)
+		{
+			trans[j][i] = a[i][j];
+
+		}
+		return trans;
+
+
 	}
 }
--- MatrixLib/MatrixLib.h
+++ MatrixLib/MatrixLib.h
@@ -11,6 +11,11 @@ namespace MatrixLib {
 		// TODO: Add your methods for this class here.
 		public:
 			// Returns a + b  
+			static int** addition(int arr1[3][3], int arr2[3][3]);
+		
+			static int** subtract(int arr3[3][3], int arr4[3][3]);
 			static int** multiply(int a[3][3], int b[3][3]);
+
+			static int**inverse(int a[3][3]);
 	};
 }
